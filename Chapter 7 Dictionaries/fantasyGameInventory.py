990f8c95f2f7a9f231ce65e45326b948bf96511c
chiitoi <kevinwwang@outlook.com>
2026-04-25 22:30:58 -0700

    reformated the strings and added a total sum of inventory items

--- Chapter 7 Dictionaries/fantasyGameInventory.py
+++ Chapter 7 Dictionaries/fantasyGameInventory.py
@@ -6,8 +6,12 @@ def add_to_inventory(inventory, added_items):
 def display_inventory(inventory):
     item_list = []
     for item, value in inventory.items():
-        item_list.append(f"{item}: {value}")
-    return "\n".join(item_list)
+        item_list.append(f"{value} {item}")
+    total_items = sum(inventory.values())
+    string = "Inventory:\n"
+    string += "\n".join(item_list)
+    string += f"\n\nTotal number of items: {total_items}"
+    return string
 
 inv = {'gold coin': 42, 'rope': 1}
 dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
