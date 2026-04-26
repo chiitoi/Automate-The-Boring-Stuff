08fb34efa3e2b6ee9380260fe79ce906b9b21a23
chiitoi <kevinwwang@outlook.com>
2026-04-25 23:02:38 -0700

    add average function

--- Chapter 7 Dictionaries/weatherDataGen.py
+++ Chapter 7 Dictionaries/weatherDataGen.py
@@ -9,8 +9,16 @@ def get_random_weather_data():
     weather['pressure'] = random.randint(990, 1010)
     return weather
 
+def get_average_temperature(weather_data):
+    total = 0
+    for weather_dict in weather_data:
+        total += weather_dict['temp']
+    return round(total / len(weather_data),1)
+
 weather_list = []
 for _ in range(100):
     weather_list.append(get_random_weather_data())
 
 pprint.pprint(weather_list)
+
+print(get_average_temperature(weather_list))
