561781b767ee30bc02c0b3e7368ededa162f6760
chiitoi <kevinwwang@outlook.com>
2026-04-27 16:40:40 -0700

    Adding comments

--- Chapter 8 Strings and Text Editing/tablePrinter.py
+++ Chapter 8 Strings and Text Editing/tablePrinter.py
@@ -8,10 +8,19 @@ def printTable(string_table):
     new_table = []
     col_widths = []
 
+    #Find the max width of each column so that we can use .rjust() for each item later
     for row in string_table:
         max_length = max(len(item) for item in row)
         col_widths.append(max_length)
     
+    #We're rotating the matrix so we first need to access the table by column first (apples, Alice, dogs) then go through each row for the individual items
+    #This is appended into a new table so that looks like the following
+    """
+      apples Alice  dogs
+     oranges   Bob  cats
+    cherries Carol moose
+      banana David goose
+    """
     for k in range(cols):
         new_row = []
         for i in range(rows):
@@ -20,6 +29,7 @@ def printTable(string_table):
         new_table.append(new_row)
 
     string = ""
+    #Joining each row into a single string then combining all the rows with a new line
     for row in new_table:
         string += " ".join(row) + "\n"
     return string
