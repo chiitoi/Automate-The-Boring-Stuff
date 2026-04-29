ad89febd3d1d79371165238ecfeda6965d88a44c
chiitoi <kevinwwang@outlook.com>
2026-04-28 20:00:23 -0700

    attempting a regex version of the strip() method

---
+++ Chapter 9 Text Pattern Matching with Regular Expressions/regexVersionStripMethod.py
@@ -0,0 +1,23 @@
+#Chapter 9 of Automate the Boring Stuff by Al Sweigart
+#https://automatetheboringstuff.com/3e/chapter9.html
+
+import re
+
+def regex_strip(sentence, characters = ""):
+    if not characters:
+        #search full string and only group the middle part of the string
+        #it will always return something
+        white_space = re.compile(r"^\s*(.*?)\s*$") 
+        contains_spaces = white_space.search(sentence)
+        return contains_spaces.group(1)
+    else:
+        custom_characters = re.compile(rf"^[{re.escape(characters)}]+|[{re.escape(characters)}]+$")
+        new_sentence = custom_characters.sub("", sentence)
+        return new_sentence
+
+string1 = " asodo "
+string2 = r",,,,,rrttg]].-\\g.....banrrrana....rrr"
+print(regex_strip(string1))
+print()
+arguments = ",.grt.]-\\"
+print(regex_strip(string2, arguments))
